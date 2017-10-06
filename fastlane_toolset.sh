a5bc21da1c6ab81716a37bb44913e8c0eede5ed4
QXSoftware <lh563566994@126.com>
2017-10-06 17:42:23 +0800

    add shell script entry

--- fastlane_toolset.sh
+++ fastlane_toolset.sh
@@ -3,4 +3,45 @@
 # skip fastlane autoupdate check
 export FASTLANE_SKIP_UPDATE_CHECK=1
 
-fastlane
+# to use /usr/local/bin/fastlane
+export PATH=/usr/local/bin/:${PATH}
+
+xcodeproj=''
+plist=''
+build_number=''
+build_version=''
+ipa_path=''
+
+while getopts "x:p:n:v:o:" opt; do
+	case $opt in
+		x)
+			xcodeproj=$OPTARG
+			;;
+		p)
+			plist=$OPTARG
+			;;
+		n)
+			build_number=$OPTARG
+			;;
+		v)
+			build_version=$OPTARG
+			;;
+		o)
+			ipa_path=$OPTARG
+			;;
+		\?)
+			exit -1
+			;;
+	esac
+done
+
+fastlane ${lane} \
+	xcodeproj:${xcodeproj} \
+	plist:${plist} \
+	build_number:${build_number} \
+	build_version:${build_version} \
+	ipa_path:${ipa_path} \
+
+if (($?)); then exit -1; fi
+
+say 'fastlane build success!'
